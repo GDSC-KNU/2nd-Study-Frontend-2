9952bde8e7b2367bf83d3311d666c42882bf126a
hwiiy <87405971+hwiiy@users.noreply.github.com>
2023-01-29 23:24:14 +0900

    반응형 일부 구현 완료 : 별 위치, Game start, Congrats 문구 수정 필요

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import GameOver from "./GameOver";
 import GameAllClear from "./GameAllClear";
 import Game from "./Game";
 import StageClear from "./StageClear";
-
+import MediaQuery from "react-responsive";
 function App() {
   localStorage.setItem("score1", 0);
   localStorage.setItem("score2", 0);
   localStorage.setItem("score3", 0);
   return (
-    <Routes>
-      <Route path="/" element={<Gamestart />} />
-      <Route path="/game/:no" element={<Game />} />
-      <Route path="/GameOver" element={<GameOver />} />
-      <Route path="/GameAllClear" element={<GameAllClear />} />
-      <Route path="/stageClear/:no" element={<StageClear />} />
-    </Routes>
+    <>
+      {/* pc */}
+      <MediaQuery minWidth={1024}>
+        <div className="game">
+          <Routes>
+            <Route path="/" element={<Gamestart />} />
+            <Route path="/game/:no" element={<Game />} />
+            <Route path="/GameOver" element={<GameOver />} />
+            <Route path="/GameAllClear" element={<GameAllClear />} />
+            <Route path="/stageClear/:no" element={<StageClear />} />
+          </Routes>
+        </div>
+      </MediaQuery>
+
+      {/* mobile */}
+      <MediaQuery minWidth={450}>
+        <div className="game">
+          <Routes>
+            <Route path="/" element={<Gamestart />} />
+            <Route path="/game/:no" element={<Game />} />
+            <Route path="/GameOver" element={<GameOver />} />
+            <Route path="/GameAllClear" element={<GameAllClear />} />
+            <Route path="/stageClear/:no" element={<StageClear />} />
+          </Routes>
+        </div>
+      </MediaQuery>
+    </>
   );
 }
 
